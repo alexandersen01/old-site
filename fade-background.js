008044b6644a5cf4d43566301cf6eaaf2ecdeae4
Jakob <jakob.sverre@gmail.com>
2023-05-06 17:51:46 +0200

    finished?

--- fade-background.js
+++ fade-background.js
@@ -2,9 +2,18 @@ const fadeOutBackground = document.querySelector('.fade-out-background');
 
 window.addEventListener('scroll', () => {
   const scrollPosition = window.pageYOffset;
-  const opacity = 1 - (scrollPosition / 200); // change 500 to adjust the fade-out speed
+  const opacity = 1 - (scrollPosition / 500); // change 500 to adjust the fade-out speed
   fadeOutBackground.style.opacity = opacity;
 });
 
-const header3 = document.querySelector('.header3');
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".topnav").classList.remove("hidden");
+  } else {
+    document.querySelector(".topnav").classList.add("hidden");
+  }
+  prevScrollpos = currentScrollPos;
+}
 

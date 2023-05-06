51db3eabc07f6a65bea067d2e6d689149568122a
Jakob <jakob.sverre@gmail.com>
2023-05-06 12:55:44 +0200

    nice

--- fade-background.js
+++ fade-background.js
@@ -5,3 +5,15 @@ window.addEventListener('scroll', () => {
   const opacity = 1 - (scrollPosition / 200); // change 500 to adjust the fade-out speed
   fadeOutBackground.style.opacity = opacity;
 });
+
+const header3 = document.querySelector('.header3');
+
+window.addEventListener('scroll', function() {
+    var header3 = document.querySelector('.header3');
+    var position = header3.getBoundingClientRect();
+  
+    // If header3 is in the viewport
+    if(position.top >= 0 && position.bottom <= window.innerHeight) {
+      header3.classList.add('visible');
+    }
+  });

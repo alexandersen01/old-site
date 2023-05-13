d532ea439fa6d6c2c2d68b75447c831a4534d8c4
Jakob <jakob.sverre@gmail.com>
2023-05-13 14:34:01 +0200

    added cavnas bot

--- fade-background.js
+++ fade-background.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', () => {
 });
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".topnav").classList.remove("hidden");
@@ -31,43 +31,46 @@ function toggleFullscreen() {
 
 document.getElementById('fullscreen-btn').addEventListener('click', toggleFullscreen);
 
-document.addEventListener('fullscreenchange', function(event) {
+document.addEventListener('fullscreenchange', function (event) {
   if (!document.fullscreenElement) {
     document.getElementById("fullscreen-btn").style.backgroundImage = "url(fullscreen.png)";
   }
 });
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape' && document.fullscreenElement) {
     document.exitFullscreen();
     document.getElementById("fullscreen-btn").style.backgroundImage = "url(fullscreen.png)";
   }
 });
 
-document.getElementById("scrollBtn").addEventListener("click", function() {
+document.getElementById("scrollBtn").addEventListener("click", function () {
   const header = document.querySelector('.header3');
   const headerRect = header.getBoundingClientRect();
   const headerTop = headerRect.top + window.scrollY;
   const scrollTop = headerTop - (headerTop * 0.03); // Scroll to 10% above the header's top
-  window.scrollTo({top: scrollTop, behavior: 'smooth'});
+  window.scrollTo({ top: scrollTop, behavior: 'smooth' });
 });
 
-document.getElementById("scrollBtn2").addEventListener("click", function() {
+document.getElementById("scrollBtn2").addEventListener("click", function () {
   const header = document.querySelector('.tetris');
   const headerRect = header.getBoundingClientRect();
   const headerTop = headerRect.top + window.scrollY;
   const scrollTop = headerTop - (headerTop * 0.01); // Scroll to 10% above the header's top
-  window.scrollTo({top: scrollTop, behavior: 'smooth'});
+  window.scrollTo({ top: scrollTop, behavior: 'smooth' });
 });
 
-document.getElementById("scrollBtn3").addEventListener("click", function() {
+document.getElementById("scrollBtn3").addEventListener("click", function () {
   const header = document.querySelector('.sudoku');
   const headerRect = header.getBoundingClientRect();
   const headerTop = headerRect.top + window.scrollY;
   const scrollTop = headerTop - (headerTop * 0.01); // Scroll to 10% above the header's top
-  window.scrollTo({top: scrollTop, behavior: 'smooth'});
+  window.scrollTo({ top: scrollTop, behavior: 'smooth' });
 });
 
-
-
-
-
+document.getElementById("scrollBtn4").addEventListener("click", function () {
+  const header = document.querySelector('.CanvasBot');
+  const headerRect = header.getBoundingClientRect();
+  const headerTop = headerRect.top + window.scrollY;
+  const scrollTop = headerTop - (headerTop * 0.01); // Scroll to 10% above the header's top
+  window.scrollTo({ top: scrollTop, behavior: 'smooth' });
+});
